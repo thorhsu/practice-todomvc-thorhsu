dceb9eaf4d0fe5cb27fd569f4145b7ce6c6a31f1
thorhsu <thorshu@gmail.com>
2019-01-18 01:40:31 +0800

    Console log the words you entered after you press enter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Todomvc';
   newPlaceHolder = 'What needs to be done?';
+
+  addTodo(template){
+    console.log(template.value);
+
+  }
 }
