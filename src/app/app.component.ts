eac0765cea51bf7cd3d827a9df2d1d8a339fe192
thorhsu <thorshu@gmail.com>
2019-01-18 02:18:16 +0800

    Set completed class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Todo {
+  id: number;
+  todoNm: string;
+  isCompleted: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +15,23 @@ export class AppComponent {
   title = 'Todomvc';
   newPlaceHolder = 'What needs to be done?';
   newTodo = '';
-  todos = [];
+  todos: Todo[] = [];
+  id = 0;
 
   addTodo() {
-    this.todos.push(this.newTodo);
-    console.log(this.todos);
+    this.todos.push(
+      {
+        id: ++this.id,
+        todoNm: this.newTodo,
+        isCompleted: false
+      }
+    );
     this.newTodo = '';
+  }
+  toggleCompleted(todo: Todo) {
+    console.log(todo);
+    todo.isCompleted = !todo.isCompleted;
+    console.log(this.todos);
+
   }
 }
