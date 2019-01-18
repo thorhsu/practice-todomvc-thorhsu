470cb496dca7a7e10f0a5a278c5ba119e37a8487
thorhsu <thorshu@gmail.com>
2019-01-18 20:57:03 +0800

    Modified by the comments

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -8,26 +8,23 @@ import { Todo } from '../app.component';
 })
 export class FiltersComponent implements OnInit {
   _filterCondition: 'all' | 'completed' | 'active';
-  @Input() todos: Todo[];
-  @Input() uncompletedCounter: number;
+  // @Input() todos: Todo[];
+  // @Input() uncompletedCounter: number;
   @Output() filterConditionChange = new EventEmitter();
   @Input() get filterCondition() {
     return this._filterCondition;
   }
-  @Output() clearCompleted = new EventEmitter();
   set filterCondition(filterCondition: 'all' | 'completed' | 'active') {
     this._filterCondition = filterCondition;
     this.filterConditionChange.emit(this._filterCondition);
   }
+  // @Output() clearCompleted = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  clearCompletedEmit() {
-    this.clearCompleted.emit();
-  }
-
-
+  // clearCompletedEmit() {
+  //   this.clearCompleted.emit();
+  // }
 }
